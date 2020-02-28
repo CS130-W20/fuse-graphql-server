d517d5c286130cb1090efb6aa79c5a1333cbf6ec
ryan macbook <rvtsang@gmail.com>
2020-02-27 23:27:52 -0800

    Add user event query without association

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -8,6 +8,40 @@ async function ownedEvents(parent, args, context) {
   });
 }
 
+// eslint-disable-next-line no-unused-vars
+async function events(parent, { userId, association, status }, context) {
+  // TODO add association support
+  return context.prisma.events({
+    where: {
+      AND: [
+        {
+          OR: [
+            {
+              owner: {
+                id: userId,
+              },
+            },
+            {
+              invited_some: {
+                id: userId,
+              },
+            },
+            {
+              joined_some: {
+                id: userId,
+              },
+            },
+          ],
+        },
+        {
+          status_in: status,
+        },
+      ],
+    },
+  });
+}
+
 export default {
   ownedEvents,
+  events,
 };
